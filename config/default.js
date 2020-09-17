ef2954ce43aa16d2f58e8e39b652aa6599778c1c
wangxuyang <wxy2335962829@sina.com>
2020-09-17 15:15:30 +0800

    配置问件修改

--- config/default.js
+++ config/default.js
@@ -4,12 +4,5 @@ module.exports = {
         secret: 'myblog',
         key: 'myblog',
         maxAge: 2592000000
-    },
-    dataBase: {
-        host: 'localhost',
-        port: 3306,
-        user: 'wangxuyang',
-        password: 'wxy123456',
-        database: 'blog'
     }
 }
